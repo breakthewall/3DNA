8c4b480a38780476866c37efbfc17bd2e1b0ee42
joan <joan.herisson@univ-evry.fr>
2020-01-15 14:44:14 +0100

    input args

--- Main.py
+++ Main.py
@@ -1,17 +1,29 @@
 from RotTable import *
 from Traj3D import *
 
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--filename", help="input filename of DNA sequence")
+parser.parse_args()
+args = parser.parse_args()
 
 def main():
 
-	rot_table = RotTable()
-	traj = Traj3D()
+    rot_table = RotTable()
+    traj = Traj3D()
 
-	traj.compute("AAAGGATCTTCTTGAGATCCTTTTTTTCTGCGCGTAATCTGCTGCCAGTAAACGAAAAAACCGCCTGGGGAGGCGGTTTAGTCGAAGGTTAAGTCAG", rot_table)
+    if args.filename:
+	    # Read file
+	    lineList = [line.rstrip('\n') for line in open("plasmid_8k.fasta")]
+		# Formatting
+	    seq = ''.join(lineList[1:])
+	    traj.compute(seq, rot_table)
+    else:
+        traj.compute("AAAGGATCTTCTTGAGATCCTTTTTTTCTGCGCGTAATCTGCTGCCAGTAAACGAAAAAACCGCCTGGGGAGGCGGTTTAGTCGAAGGTTAAGTCAG", rot_table)
 
-	print(traj.getTraj())
+    print(traj.getTraj())
 
-	traj.draw("test.png")
+    traj.draw("test.png")
 
 
 if __name__ == "__main__" :
