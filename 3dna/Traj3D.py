94912b3aabd5208f76e1b587cbd1a7d3c23114c2
Joan Hérisson <joan.herisson@univ-evry.fr>
2024-01-29 16:55:48 +0100

    chore(Traj3D.py): rm mathutils

--- 3dna/Traj3D.py
+++ 3dna/Traj3D.py
@@ -1,8 +1,3 @@
-#For computing
-from mathutils import (
-    Matrix,
-    Vector
-)
 import math
 
 #For drawing
@@ -16,7 +11,10 @@ class Traj3D:
     """Represents a 3D trajectory"""
 
     # Vertical translation (elevation) between two di-nucleotides
-    __MATRIX_T = Matrix.Translation((0.0, 0.0, 3.38/2.0, 1.0))
+    __MATRIX_T = np.array([[1, 0, 0, 0],
+                       [0, 1, 0, 0],
+                       [0, 0, 1, 3.38/2.0],
+                       [0, 0, 0, 1]])
 
     def __init__(self):
         self.__Traj3D = {}
@@ -29,10 +27,10 @@ class Traj3D:
     def compute(self, dna_seq: str, rot_table: RotTable):
 
         # Matrice cumulant l'ensemble des transformations géométriques engendrées par la séquence d'ADN
-        total_matrix = Matrix()
+        total_matrix = np.eye(4)  # Identity matrix
 
         # On enregistre la position du premier nucléotide
-        self.__Traj3D = [Vector((0.0, 0.0, 0.0, 1.0))]
+        self.__Traj3D = [np.array([0.0, 0.0, 0.0, 1.0])]
 
         matrices_Rz = {}
         matrices_Q = {}
@@ -42,33 +40,39 @@ class Traj3D:
             dinucleotide = dna_seq[i-1]+dna_seq[i]
             # On remplit au fur et à mesure les matrices de rotation
             if dinucleotide not in matrices_Rz:
-                matrices_Rz[dinucleotide] = Matrix.Rotation(
-                    math.radians(rot_table.getTwist(dinucleotide)/2),
-                    4,
-                    'Z'
-                )
+                theta = math.radians(rot_table.getTwist(dinucleotide)/2)
+                # Create rotation matrix of theta on Z axis
+                matrices_Rz[dinucleotide] = \
+                    np.array([[math.cos(theta), -math.sin(theta), 0, 0],
+                              [math.sin(theta), math.cos(theta), 0, 0],
+                              [0, 0, 1, 0],
+                              [0, 0, 0, 1]])
+
+                alpha = math.radians((rot_table.getWedge(dinucleotide)))
+                beta = math.radians((rot_table.getDirection(dinucleotide)-90))
+                # Rotate of -beta on Z axis
+                # Rotate of -alpha on X axis
+                # Rotate of beta on Z axis
                 matrices_Q[dinucleotide] = \
-                    Matrix.Rotation(
-                        math.radians((rot_table.getDirection(dinucleotide)-90)),
-                        4,
-                        'Z'
-                    ) \
-                    @ Matrix.Rotation(
-                        math.radians((-rot_table.getWedge(dinucleotide))),
-                        4,
-                        'X'
-                    ) \
-                    @ Matrix.Rotation(
-                        math.radians((90-rot_table.getDirection(dinucleotide))),
-                        4,
-                        'Z'
-                    )
+                    np.array([[math.cos(-beta), -math.sin(-beta), 0, 0],
+                              [math.sin(-beta), math.cos(-beta), 0, 0],
+                              [0, 0, 1, 0],
+                              [0, 0, 0, 1]]) \
+                    @ np.array([[1, 0, 0, 0],
+                                 [0, math.cos(-alpha), -math.sin(-alpha), 0],
+                                 [0, math.sin(-alpha), math.cos(-alpha), 0],
+                                 [0, 0, 0, 1]]) \
+                    @ np.array([[math.cos(beta), -math.sin(beta), 0, 0],
+                              [math.sin(beta), math.cos(beta), 0, 0],
+                              [0, 0, 1, 0],
+                              [0, 0, 0, 1]])
 
             # On calcule les transformations géométriques
             # selon le dinucleotide courant,
             # et on les ajoute à la matrice totale
-            total_matrix @= \
-                self.__MATRIX_T \
+            total_matrix = \
+                total_matrix \
+                @ self.__MATRIX_T \
                 @ matrices_Rz[dinucleotide] \
                 @ matrices_Q[dinucleotide] \
                 @ matrices_Rz[dinucleotide] \
